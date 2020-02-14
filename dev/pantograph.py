4400480b67cf56f9ab5c6a3b0eb111dbd733e85a
jdecker77 <jdecker77@gmail.com>
2020-02-13 19:10:58 -0500

    Added Images

--- dev/pantograph.py
+++ dev/pantograph.py
@@ -41,12 +41,22 @@ ROOT_DIR = os.path.abspath("../")
 if ROOT_DIR not in sys.path:
     sys.path.append(ROOT_DIR)
 
+from mrcnn.config import Config
+from mrcnn import model as modellib, utils
+
+
+
+tools = os.path.abspath("../../")
+tools = tools+'/coco-master/PythonAPI'
+if tools not in sys.path:
+    sys.path.append(tools)
+    
+# for i in sys.path:
+#     print(i)
+
 from cocotools.coco import COCO
 from cocotools.cocoeval import COCOeval
 from cocotools import mask as maskUtils
-
-from mrcnn.config import Config
-from mrcnn import model as modellib, utils
 
 
 # # Directory to save logs and trained model
@@ -529,96 +539,96 @@ def detect_and_color_splash(model, image_path=None, video_path=None):
 #  Training
 ############################################################
 
-if __name__ == '__main__':
-    import argparse
+# if __name__ == '__main__':
+#     import argparse
 
-    # Parse command line arguments
-    parser = argparse.ArgumentParser(
-        description='Train Mask R-CNN to detect pantographs.')
-    parser.add_argument("command",
-                        metavar="<command>",
-                        help="'train' or 'splash'")
-    parser.add_argument('--dataset', required=False,
-                        metavar="/path/to/pantograph/dataset/",
-                        help='Directory of the pantograph dataset')
-    parser.add_argument('--weights', required=True,
-                        metavar="/path/to/weights.h5",
-                        help="Path to weights .h5 file or 'coco'")
-    parser.add_argument('--logs', required=False,
-                        default=DEFAULT_LOGS_DIR,
-                        metavar="/path/to/logs/",
-                        help='Logs and checkpoints directory (default=logs/)')
-    parser.add_argument('--image', required=False,
-                        metavar="path or URL to image",
-                        help='Image to apply the color splash effect on')
-    parser.add_argument('--video', required=False,
-                        metavar="path or URL to video",
-                        help='Video to apply the color splash effect on')
-    args = parser.parse_args()
+#     # Parse command line arguments
+#     parser = argparse.ArgumentParser(
+#         description='Train Mask R-CNN to detect pantographs.')
+#     parser.add_argument("command",
+#                         metavar="<command>",
+#                         help="'train' or 'splash'")
+#     parser.add_argument('--dataset', required=False,
+#                         metavar="/path/to/pantograph/dataset/",
+#                         help='Directory of the pantograph dataset')
+#     parser.add_argument('--weights', required=True,
+#                         metavar="/path/to/weights.h5",
+#                         help="Path to weights .h5 file or 'coco'")
+#     parser.add_argument('--logs', required=False,
+#                         default=DEFAULT_LOGS_DIR,
+#                         metavar="/path/to/logs/",
+#                         help='Logs and checkpoints directory (default=logs/)')
+#     parser.add_argument('--image', required=False,
+#                         metavar="path or URL to image",
+#                         help='Image to apply the color splash effect on')
+#     parser.add_argument('--video', required=False,
+#                         metavar="path or URL to video",
+#                         help='Video to apply the color splash effect on')
+#     args = parser.parse_args()
 
-    # Validate arguments
-    if args.command == "train":
-        assert args.dataset, "Argument --dataset is required for training"
-    elif args.command == "splash":
-        assert args.image or args.video,\
-               "Provide --image or --video to apply color splash"
+#     # Validate arguments
+#     if args.command == "train":
+#         assert args.dataset, "Argument --dataset is required for training"
+#     elif args.command == "splash":
+#         assert args.image or args.video,\
+#                "Provide --image or --video to apply color splash"
 
-    print("Weights: ", args.weights)
-    print("Dataset: ", args.dataset)
-    print("Logs: ", args.logs)
+#     print("Weights: ", args.weights)
+#     print("Dataset: ", args.dataset)
+#     print("Logs: ", args.logs)
 
-    # Configurations
-    if args.command == "train":
-        config = PantographConfig()
-    else:
-        class InferenceConfig(PantographConfig):
-            # Set batch size to 1 since we'll be running inference on
-            # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
-            GPU_COUNT = 1
-            IMAGES_PER_GPU = 1
-        config = InferenceConfig()
-    config.display()
+#     # Configurations
+#     if args.command == "train":
+#         config = PantographConfig()
+#     else:
+#         class InferenceConfig(PantographConfig):
+#             # Set batch size to 1 since we'll be running inference on
+#             # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+#             GPU_COUNT = 1
+#             IMAGES_PER_GPU = 1
+#         config = InferenceConfig()
+#     config.display()
 
-    # Create model
-    if args.command == "train":
-        model = modellib.MaskRCNN(mode="training", config=config,
-                                  model_dir=args.logs)
-    else:
-        model = modellib.MaskRCNN(mode="inference", config=config,
-                                  model_dir=args.logs)
+#     # Create model
+#     if args.command == "train":
+#         model = modellib.MaskRCNN(mode="training", config=config,
+#                                   model_dir=args.logs)
+#     else:
+#         model = modellib.MaskRCNN(mode="inference", config=config,
+#                                   model_dir=args.logs)
 
-    # Select weights file to load
-    if args.weights.lower() == "coco":
-        weights_path = COCO_WEIGHTS_PATH
-        # Download weights file
-        if not os.path.exists(weights_path):
-            utils.download_trained_weights(weights_path)
-    elif args.weights.lower() == "last":
-        # Find last trained weights
-        weights_path = model.find_last()
-    elif args.weights.lower() == "imagenet":
-        # Start from ImageNet trained weights
-        weights_path = model.get_imagenet_weights()
-    else:
-        weights_path = args.weights
+#     # Select weights file to load
+#     if args.weights.lower() == "coco":
+#         weights_path = COCO_WEIGHTS_PATH
+#         # Download weights file
+#         if not os.path.exists(weights_path):
+#             utils.download_trained_weights(weights_path)
+#     elif args.weights.lower() == "last":
+#         # Find last trained weights
+#         weights_path = model.find_last()
+#     elif args.weights.lower() == "imagenet":
+#         # Start from ImageNet trained weights
+#         weights_path = model.get_imagenet_weights()
+#     else:
+#         weights_path = args.weights
 
-    # Load weights
-    print("Loading weights ", weights_path)
-    if args.weights.lower() == "coco":
-        # Exclude the last layers because they require a matching
-        # number of classes
-        model.load_weights(weights_path, by_name=True, exclude=[
-            "mrcnn_class_logits", "mrcnn_bbox_fc",
-            "mrcnn_bbox", "mrcnn_mask"])
-    else:
-        model.load_weights(weights_path, by_name=True)
+#     # Load weights
+#     print("Loading weights ", weights_path)
+#     if args.weights.lower() == "coco":
+#         # Exclude the last layers because they require a matching
+#         # number of classes
+#         model.load_weights(weights_path, by_name=True, exclude=[
+#             "mrcnn_class_logits", "mrcnn_bbox_fc",
+#             "mrcnn_bbox", "mrcnn_mask"])
+#     else:
+#         model.load_weights(weights_path, by_name=True)
 
-    # Train or evaluate
-    if args.command == "train":
-        train(model)
-    elif args.command == "splash":
-        detect_and_color_splash(model, image_path=args.image,
-                                video_path=args.video)
-    else:
-        print("'{}' is not recognized. "
-              "Use 'train' or 'splash'".format(args.command))
+#     # Train or evaluate
+#     if args.command == "train":
+#         train(model)
+#     elif args.command == "splash":
+#         detect_and_color_splash(model, image_path=args.image,
+#                                 video_path=args.video)
+#     else:
+#         print("'{}' is not recognized. "
+#               "Use 'train' or 'splash'".format(args.command))
--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -571,67 +571,6 @@ class Dataset(object):
         return keypoints, mask, class_ids
 
 
-# def resize_image(image, min_dim=None, max_dim=None, padding=False, mode="square"):
-    """
-    Resizes an image keeping the aspect ratio.
-
-    min_dim: if provided, resizes the image such that it's smaller
-        dimension == min_dim
-    max_dim: if provided, ensures that the image longest side doesn't
-        exceed this value.
-    padding: If true, pads image with zeros so it's size is max_dim x max_dim
-
-    Returns:
-    image: the resized image
-    window: (y1, x1, y2, x2). If max_dim is provided, padding might
-        be inserted in the returned image. If so, this window is the
-        coordinates of the image part of the full image (excluding
-        the padding). The x2, y2 pixels are not included.
-    scale: The scale factor used to resize the image
-    padding: Padding added to the image [(top, bottom), (left, right), (0, 0)]
-    """
-    # Default window (y1, x1, y2, x2) and default scale == 1.
-    # h, w = image.shape[:2]
-    # window = (0, 0, h, w)
-    # scale = 1
-    # padding = [(0, 0), (0, 0), (0, 0)]
-    # crop = None
-
-
-    # # if mode == "none":
-    # #     return image, window, scale, padding
-
-    # # Scale?
-    # if min_dim:
-    #     # Scale up but not down
-    #     scale = max(1, min_dim / min(h, w))
-    # # Does it exceed max dim?
-    # if max_dim:
-    #     image_max = max(h, w)
-    #     if round(image_max * scale) > max_dim:
-    #         scale = max_dim / image_max
-    # # Resize image and mask
-    # if scale != 1:
-    #     image = np.array(Image.fromarray(image).resize((round(w * scale),round(h * scale))))
-    #     # np.array(Image.fromarray(round(h * scale),round(w * scale)).resize())
-    #     # image = scipy.misc.imresize(image, (round(h * scale), round(w * scale)))
-    
-    # # Need padding?
-    # if padding:
-    #     # Get new height and width
-    #     h, w = image.shape[:2]
-    #     top_pad = (max_dim - h) // 2
-    #     bottom_pad = max_dim - h - top_pad
-    #     left_pad = (max_dim - w) // 2
-    #     right_pad = max_dim - w - left_pad
-    #     padding = [(top_pad, bottom_pad), (left_pad, right_pad), (0, 0)]
-    #     image = np.pad(image, padding, mode='constant', constant_values=0)
-    #     window = (top_pad, left_pad, h + top_pad, w + left_pad)
-    #     print('padding',padding)
-    # return image, window, scale, padding
-    # image.astype(image_dtype), window, scale, padding, crop
-
-
 def resize_mask(mask, scale, padding):
     """Resizes a mask using the given scale and padding.
     Typically, you get the scale and padding from resize_image() to
